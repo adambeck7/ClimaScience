be860c32f5ea1adc8231eb1393a79ecef6721b53
adambeck7 <adambeck7@gmail.com>
2018-08-30 11:35:13 -0600

    added routes and got express running

--- server.js
+++ server.js
@@ -22,8 +22,6 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
-require("./routes/author-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
